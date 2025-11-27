3cc2689a6c802bf469689f2d5d8c55410bbaae12
sudrajatarif <sudrajat.arif.2505356@students.um.ac.id>
2025-11-27 14:17:19 +0700

    Update define

--- Kernel/sys/syscall.c
+++ Kernel/sys/syscall.c
@@ -2,8 +2,11 @@
 #include "syscall.h"
 #include "driver.h"
 
-void init_syscall() { }
+#define SYS_WRITE    0
+#define SYS_READ     1
+#define SYS_OPEN     2
+#define SYS_CLOSE    3
+#define SYS_GETSTAT  4
+#define SYS_PRINT    5
+#define SYS_EXIT     6
 
-void sys_print(const char *s) {
-    driver_write(s);
-}
